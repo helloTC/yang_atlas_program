d61e837b151a7e2e2097649d6c22a29b4e51087f
helloTC <taicheng_huang@sina.cn>
2015-11-02 11:23:43 +0800

    add notes

--- atlas/atlas/datadeposite.py
+++ atlas/atlas/datadeposite.py
@@ -2,22 +2,33 @@ import sys
 import types
 
 class DataDeposite(object):
+# This class's main function is to initial variables in __dict__
+
+# _attrs is an attribute deposite,every attribute we want to add will 
+# firstly add into this varibles.
+# _nodump_attrs is a temporal attribute deposite,it can only save some 
+# temporal attributes which may brush off finally
     _attrs = []
     _nodump_attrs = []
-
+# init DataDeposite.First update attributes and values in this deposite.
+# If attributes were not in __dict__,but in _attrs and _nodump_attrs,add it,
+# and assign it to None
     def __init__(self, **entries):
         self.__dict__.update(entries)
         for key in (self._attrs + self._nodump_attrs):
             if key not in self.__dict__:
                 self.__dict__.update({key:None})
-
+# refresh keys in self.__dict__(attention:that's not means change attributes 
+# in __dict__,it firstly copy attributes in __dict__ to odict,then refresh keys
+# in odict)
+# if keys in odict are no longer in _attrs,delete it 
     def __getstate__(self):
         odict = self.__dict__.copy()
         for key in odict.keys():
             if key not in self._attrs:
                 del odict[key]
         return odict 
-
+# Redirect __repr__,show attributes and values in _attrs and _nodump_attrs
     def __repr__(self):
         args = [ '%s=%s' % (k, vars(self).get(k)) for k in self._attrs + self._nodump_attrs]
         return 'DataDeposite(%s)' % ', '.join(args)
