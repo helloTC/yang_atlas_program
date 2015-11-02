a1892615c76242828aa43dd31e891d1cfcff9767
helloTC <taicheng_huang@sina.cn>
2015-11-02 13:33:10 +0800

    add notes

--- atlas/atlas/datadeposite.py
+++ atlas/atlas/datadeposite.py
@@ -35,57 +35,67 @@ class DataDeposite(object):
 
 
 class DataDepositeTk(object):
+# Toolkit for DataDeposite,including some operations
+
+# Create attribute depo for saving varibles in depo
+# if no varibles in depo,run depocls to add varibles
     def __init__(self, depo=None, depocls=None, *args, **kargs):
         if depo is not None:
             self.depo = depo
         else:
             self.depo = depocls(*args, **kargs)
 
+# return depo
     def getdepo(self):
         return self.depo
-    
+# set depo into self.depo     
     def setdepo(self, depo):
         self.depo = depo
-
+# transform attributes and values from self.depo into __dict__? I guess
     def deposite(self, value, name, cache=True):
         if cache:
             setattr(self.depo, name, value)
-        
+# clear attributes in __dict__ and _nodump_attrs,delete these keys
     def clear_nodump(self):
         for key in self.depo._nodump_attrs:
             if key in self.depo.__dict__:
                 del self.depo.__dict__[key]
 
-
 class DataDepositeDecorator(DataDepositeTk):
+# Decorate deposite 
+
+# add attributes and values into self.depo
     def add_attr(self, name, value):
         setattr(self.depo, name, value)
-
+# redirect __dir__,return attributes in __dict__ and in depo.__dict__
     def __dir__(self):
         return self.__dict__.keys() + self.depo.__dict__.keys()
-
+# get attributes from self.depo
     def __getattr__(self, key):
         if hasattr(self.depo, key):
             return getattr(self.depo, key)
         else:
             raise AttributeError, key
-
-
+# screen data type
 def DataTypedef(cls, attrs):
     return type(cls, (DataDeposite,), {'_attrs':attrs})
 
 
 class Struct:
+# define a class named Struct,with method of initialization and representation
+
+# init Struct class
     def __init__(self, **entries):
         self.__dict__.update(entries)
-
+# screen attributes
     def __repr__(self):
         args = [ '%s=%s' % (k, repr(v)) for (k,v) in vars(self).items()]
         return 'Struct(%s)' % ', '.join(args)
 
+# Load modules into memory,and update attributes in struct into __main__
 def dump_struct_to_main(struct):
     sys.modules['__main__'].__dict__.update(struct.__dict__)
-
+# Update attributes and values
 def update(x, **entries):
     if type(x) == types.DictType:
         x.update(entries)
